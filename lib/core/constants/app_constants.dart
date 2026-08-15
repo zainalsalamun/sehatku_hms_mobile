4d0f6606d0c4d945c3a5833fc0dd037a2c42ab0d
zainalsalamun <zainalrtf@gmail.com>
2026-08-15 12:06:47 +0700

    chore: install missing project dependencies in node_modules

--- lib/core/constants/app_constants.dart
+++
@@ -1,8 +0,0 @@
-abstract final class AppConstants {
-  static const appName = 'SehatKu';
-  static const hospitalName = 'SehatKu Medical Center';
-  static const apiBaseUrl = String.fromEnvironment(
-    'API_BASE_URL',
-    defaultValue: 'https://api.example.com/v1',
-  );
-}
